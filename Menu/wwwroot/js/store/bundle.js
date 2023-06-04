90dee6add9997b7815212b270912be4668242c01
zsuzitor <avdosevstas@mail.ru>
2023-06-04 17:27:29 +0300

    поправил копирование

--- Menu/wwwroot/js/store/bundle.js
+++ Menu/wwwroot/js/store/bundle.js
@@ -56800,6 +56800,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -56812,6 +56848,7 @@ var CreateVault_1 = __importDefault(__webpack_require__(/*! ../CreateVault/Creat
 var AdditionalWindow_1 = __importDefault(__webpack_require__(/*! ../../../../components/Body/AdditionalWindow/AdditionalWindow */ "./src/components/Body/AdditionalWindow/AdditionalWindow.tsx"));
 var AlertData_1 = __webpack_require__(/*! ../../../../Models/Entity/AlertData */ "./src/Models/Entity/AlertData.ts");
 var CreateSecret_1 = __importDefault(__webpack_require__(/*! ../CreateSecret/CreateSecret */ "./src/Apps/Vault/Components/CreateSecret/CreateSecret.tsx"));
+var Helper_1 = __webpack_require__(/*! ../../../../Models/BL/Helper */ "./src/Models/BL/Helper.ts");
 __webpack_require__(/*! ./OneVault.css */ "./src/Apps/Vault/Components/OneVault/OneVault.css");
 var OneVault = function (props) {
     // const [code, setCode] = useState('');
@@ -56821,6 +56858,7 @@ var OneVault = function (props) {
     // const [shoNewSecretForm, setShoNewSecretForm] = useState(false);
     // let vault = props.Vaults.find(x => x.Id === props.VaultId);
     var vault = props.Vault;
+    var helper = new Helper_1.Helper();
     (0, react_1.useEffect)(function () {
         //if (!props.VaultId)
         {
@@ -56880,11 +56918,20 @@ var OneVault = function (props) {
                             }
                         } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'delete-icon.png' })),
-                    react_1.default.createElement("div", { className: 'but', title: '\u0421\u0433\u0435\u043D\u0435\u0440\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u043D\u043E\u0432\u044B\u0439 \u043F\u0430\u0440\u043E\u043B\u044C', onClick: function () {
-                            var passLen = Math.floor(Math.random() * 5) + 13; //генерим от 0 до y-1, и прибавляем x, получаем от 0+x до y-1+x
-                            navigator.clipboard.writeText(passwordGenerate(10));
-                            G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Скопировано"));
-                        } },
+                    react_1.default.createElement("div", { className: 'but', title: '\u0421\u0433\u0435\u043D\u0435\u0440\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u043D\u043E\u0432\u044B\u0439 \u043F\u0430\u0440\u043E\u043B\u044C', onClick: function () { return __awaiter(void 0, void 0, void 0, function () {
+                            var passLen;
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0:
+                                        passLen = Math.floor(Math.random() * 5) + 13;
+                                        return [4 /*yield*/, helper.CopyText(passwordGenerate(passLen))];
+                                    case 1:
+                                        _a.sent();
+                                        G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Скопировано"));
+                                        return [2 /*return*/];
+                                }
+                            });
+                        }); } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'key.png' })))),
             props.VaultIsAuthorized ? react_1.default.createElement(react_1.default.Fragment, null) : react_1.default.createElement("div", { className: 'vault-auth' },
                 react_1.default.createElement("input", { type: 'password', className: 'form-control', value: vaultPassword, onChange: function (e) {
@@ -57178,6 +57225,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -57278,10 +57361,17 @@ var VaultSecret = function (props) {
                 react_1.default.createElement("div", { className: 'vault-secret-main-buttons' },
                     react_1.default.createElement("div", { className: 'but vault-secret-show-val', title: showValueTitle, onClick: function () { return setShowSecretValue(!showSecretValue); } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + showValueImage })),
-                    react_1.default.createElement("div", { className: 'but vault-secret-copy', title: '\u0421\u043A\u043E\u043F\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u0437\u043D\u0430\u0447\u0435\u043D\u0438\u0435', onClick: function () {
-                            navigator.clipboard.writeText(secretValue);
-                            G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Скопировано"));
-                        } },
+                    react_1.default.createElement("div", { className: 'but vault-secret-copy', title: '\u0421\u043A\u043E\u043F\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u0437\u043D\u0430\u0447\u0435\u043D\u0438\u0435', onClick: function () { return __awaiter(void 0, void 0, void 0, function () {
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0: return [4 /*yield*/, helper.CopyText(secretValue)];
+                                    case 1:
+                                        _a.sent();
+                                        G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Скопировано"));
+                                        return [2 /*return*/];
+                                }
+                            });
+                        }); } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'copy.png' })),
                     react_1.default.createElement("div", { className: 'but vault-secret-more-but' + showMoreButtonClass, title: '\u041F\u043E\u043A\u0430\u0437\u0430\u0442\u044C \u0431\u043E\u043B\u044C\u0448\u0435', onClick: function () { return setShowMoreInfo(!showMoreInfo); } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'arrow2.png' })))),
@@ -57330,12 +57420,18 @@ var VaultSecret = function (props) {
                             return props.DeleteSecret(secret.Id, secret.VaultId);
                         } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'delete-icon.png' })),
-                    react_1.default.createElement("div", { className: 'but', title: '\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u0441\u0435\u043A\u0440\u0435\u0442', onClick: function () {
-                            navigator.clipboard
-                                .writeText(document.location.origin + G_VaultController.RouteUrlVaultApp
-                                + G_VaultController.RouteUrlOneSecret + secret.Id);
-                            G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Ссылка Скопирована"));
-                        } },
+                    react_1.default.createElement("div", { className: 'but', title: '\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u0441\u0435\u043A\u0440\u0435\u0442', onClick: function () { return __awaiter(void 0, void 0, void 0, function () {
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0: return [4 /*yield*/, helper.CopyText(document.location.origin + G_VaultController.RouteUrlVaultApp
+                                            + G_VaultController.RouteUrlOneSecret + secret.Id)];
+                                    case 1:
+                                        _a.sent();
+                                        G_AddAbsoluteAlertToState(new AlertData_1.AlertData().GetDefaultNotify("Ссылка Скопирована"));
+                                        return [2 /*return*/];
+                                }
+                            });
+                        }); } },
                         react_1.default.createElement("img", { className: 'persent-100-width-height', src: "/images/" + 'share-icon.png' })))) : react_1.default.createElement(react_1.default.Fragment, null)));
 };
 exports["default"] = (0, VaultSecretSetup_1.default)(VaultSecret);
@@ -61059,10 +61155,46 @@ exports.MainErrorHandler = MainErrorHandler;
 /*!*********************************!*\
   !*** ./src/Models/BL/Helper.ts ***!
   \*********************************/
-/***/ ((__unused_webpack_module, exports) => {
+/***/ (function(__unused_webpack_module, exports) {
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.Helper = void 0;
 var Helper = /** @class */ (function () {
@@ -61083,6 +61215,46 @@ var Helper = /** @class */ (function () {
             return num;
         }
         return '0' + numStr;
+    };
+    Helper.prototype.CopyText = function (text) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!navigator.clipboard) {
+                            // if (true) {
+                            this.fallbackCopyTextToClipboard(text);
+                            return [2 /*return*/];
+                        }
+                        return [4 /*yield*/, navigator.clipboard.writeText(text)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Helper.prototype.fallbackCopyTextToClipboard = function (text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        // Avoid scrolling to bottom
+        textArea.style.top = "-100";
+        textArea.style.left = "-100";
+        textArea.style.position = "absolute";
+        textArea.style.opacity = '0';
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        var retult = false;
+        try {
+            var successful = document.execCommand('copy');
+            retult = true;
+        }
+        catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+        document.body.removeChild(textArea);
+        return retult;
     };
     return Helper;
 }());
